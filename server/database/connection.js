df0273e2892c0c19fd6b8356c780cf0490d01720
woengus <olivierbiard@woengus.com>
2023-04-28 15:38:30 +0200

    connexion to DB

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,13 +1,15 @@
-const mongoose = require('mongoose')
-const databaseUrl =
-  process.env.DATABASE_URL || 'mongodb://localhost/argentBankDB'
+const mongoose = require("mongoose");
+const databaseUrl = process.env.DATABASE_URL;
 
 module.exports = async () => {
   try {
-    await mongoose.connect(databaseUrl, { useNewUrlParser: true })
-    console.log('Database successfully connected')
+    await mongoose.connect(
+      "mongodb+srv://Olivier:reactprojet12@cluster0.gohi3fa.mongodb.net/?retryWrites=true&w=majority",
+      { useNewUrlParser: true }
+    );
+    console.log("Database successfully connected");
   } catch (error) {
-    console.error(`Database Connectivity Error: ${error}`)
-    throw new Error(error)
+    console.error(`Database Connectivity Error: ${error}`);
+    throw new Error(error);
   }
-}
+};
